a28e9829da3fe5e9e4e36cd88d08b8220b990917
Abriel Cleaver <abrielcleaver@gmail.com>
2021-12-06 21:01:54 -0700

    create blog.js file and add imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+
+import { getBlogs } from './services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
   return (
