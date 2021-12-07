6eefade7799d57021de152bcd95ae09f7acb85ab
Abriel Cleaver <abrielcleaver@gmail.com>
2021-12-06 21:14:03 -0700

    added map

--- src/App.js
+++ src/App.js
@@ -7,9 +7,22 @@ import { getBlogs } from './services/blogs';
 import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getBlogs();
+      setBlogs(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <section className="main">
       <Header />
+      {blogs.map((blog) => (
+        <BlogCard {...blog} key={blog.id} />
+      ))}
       <Footer />
     </section>
   );
